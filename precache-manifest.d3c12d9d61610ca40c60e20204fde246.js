1a9bf6df7f37e3550d42cdc871073faf643f9444
DPadillaNYG <DPadilla.NYG@gmail.com>
2019-05-30 11:44:49 -0400

    Updates

---
+++ precache-manifest.d3c12d9d61610ca40c60e20204fde246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5d6a096217bcc0b9ff8282b0bbc07e",
+    "url": "/SkyTidings/index.html"
+  },
+  {
+    "revision": "7e7cc7267e03e223fc0e",
+    "url": "/SkyTidings/static/css/main.51ec8560.chunk.css"
+  },
+  {
+    "revision": "0de602e891c8a29df27a",
+    "url": "/SkyTidings/static/js/2.978ea561.chunk.js"
+  },
+  {
+    "revision": "7e7cc7267e03e223fc0e",
+    "url": "/SkyTidings/static/js/main.615fc2d5.chunk.js"
+  },
+  {
+    "revision": "394bbef333ae9b0e1d38",
+    "url": "/SkyTidings/static/js/runtime~main.a9034635.js"
+  }
+]);
